56b1fa8029eab4a3d8021b3552a9c253326eca32
Muneef M <muneez4us@icloud.com>
2016-11-29 03:51:27 +0530

    preferance model add

---
+++ database/migrations/2016_11_28_123830_create_leavetype_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeavetypeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+    public function up()
+    {
+        //
+        Schema::create('leavetype', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('lid');
+            // $table->string('name')->nullable();
+
+            $table->string('name')->nullable();
+            $table->string('limit')->nullable();
+            //$table->string('leave_type');
+
+
+           // $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('leavetype');
+
+    }
+
+}
